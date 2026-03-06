e20d7be019e563f8dda90711f1140f93a3166ab3
orlandotellez <espadachinkinno@gmail.com>
2026-03-05 20:49:50 -0600

    feat: agregar configuracion de redis e instalar ioredis como dependencia

---
+++ backend/src/infrastructure/redis.ts
@@ -0,0 +1,18 @@
+import Redis from "ioredis";
+import { env } from "../config/env";
+
+const redisConfig = {
+  host: env?.REDIS_HOST,
+  port: env?.REDIS_PORT,
+  maxRetriesPerRequest: 1,
+  enableOfflineQueue: false,
+  retryStrategy: () => null,
+  reconnectOnError: () => false
+}
+
+export const redis = new Redis(redisConfig)
+
+redis.on("error", (_error) => { })
+
+
+export const isRedisConnected = () => redis.status === "ready"
