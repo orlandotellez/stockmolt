bafe5d28074cb90982ec549c53231781047eca9c
orlandotellez <espadachinkinno@gmail.com>
2026-03-06 21:19:57 -0600

    feat: agregar decorador de autenticacion con fastify y tambien tipar y declarar un nuevo metodo en la interfaz de la instancia de fastify

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,30 @@
-import Fastify from "fastify";
+import Fastify from "fastify"
+import type { FastifyRequest, FastifyReply } from "fastify"
+import jwt from "@fastify/jwt"
+import { env } from "./config/env.ts"
 
-const PORT = process.env.PORT || 3000
+const PORT = env?.PORT || 3000
+
+declare module "fastify" {
+  interface FastifyInstance {
+    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>
+  }
+}
 
 const fastify = Fastify({
   logger: true
+})
+
+await fastify.register(jwt, {
+  secret: env?.JWT_SECRET as string
+})
+
+fastify.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    await request.jwtVerify()
+  } catch (error) {
+    reply.send(error)
+  }
 })
 
 fastify.get("/", (_req, reply) => {
